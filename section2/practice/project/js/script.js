f371369da54ab0404835866a9b2136921cc4675b
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-03-29 16:21:31 +0300

    Урок 30. Еще вариант

--- section2/practice/project/js/script.js
+++ section2/practice/project/js/script.js
@@ -39,21 +39,31 @@ genre.innerHTML = 'Драма'; // 2) Изменить жанр фильма, п
 
 bg.style.cssText = "background: url(../img/bg.jpg) top center/cover no-repeat;"; // 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img. Реализовать только при помощи JS
 
+// listItems.forEach(listItem => {
+// 	listItem.remove();
+// });
 
-listItems.forEach(listItem => {
-	listItem.remove();
+// const listCreate = () => {
+// 	for (let i = 0; i < sortedList.length; i++) {
+// 		let li = document.createElement('li');
+
+// 		li.classList.add('promo__interactive-item');
+
+// 		list.append(li);
+
+// 		li.innerHTML = `${i + 1}. ${sortedList[i]}`; // 5) Добавить нумерацию выведенных фильмов
+// 	}
+// };
+
+// listCreate(); // 4) Список фильмов на странице сформировать на основании данных из этого JS файла
+
+list.innerHTML = '';
+
+movieDB.movies.forEach((film, i) => {
+	list.innerHTML += `
+		<li class="promo__interactive-item">
+			${i + 1}. ${film}
+			<div class="delete"></div>
+		</li>
+	`;
 });
-
-const listCreate = () => {
-	for (let i = 0; i < sortedList.length; i++) {
-		let li = document.createElement('li');
-
-		li.classList.add('promo__interactive-item');
-
-		list.append(li);
-
-		li.innerHTML = `${i + 1}. ${sortedList[i]}`; // 5) Добавить нумерацию выведенных фильмов
-	}
-};
-
-listCreate(); // 4) Список фильмов на странице сформировать на основании данных из этого JS файла
