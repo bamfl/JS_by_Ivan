0e0c6268b1a9ee5975a1911df531d6b799e49822
dmitriypetukhov <pdnsite@gmail.com>
2021-05-10 21:23:26 +0300

    66. Создаем калькулятор на сайте, часть 1

--- section3/projects/Food/js/script.js
+++ section3/projects/Food/js/script.js
@@ -415,4 +415,76 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 		renderSlide(counter);
 	});
+
+	// 66. Создаем калькулятор на сайте, часть 1
+	let calculatingResult = document.querySelector('.calculating__result span'),
+			sex = document.querySelector('#gender'),
+			activity = document.querySelector('#activity'),
+			height = document.querySelector('#height'),
+			weight = document.querySelector('#weight'),
+			age = document.querySelector('#age'),
+			sexValue = 'female',
+			activityValue = 1.375,
+			heightValue = 160,
+			weightValue = 50,
+			ageValue = 25;
+
+	const calcResult = (sexValue, height, weight, age, activityValue) => {
+		console.log(sexValue, heightValue, weightValue, ageValue, activityValue);
+
+		if (sexValue && height && weight && age && activityValue) {
+			if (sexValue === 'female') {
+				calculatingResult.innerHTML = +((447.6 + (9.2 * weightValue) + (3.1 * heightValue) - (4.3 * ageValue)) * activityValue).toFixed(0);
+			} else if ((sexValue === 'male')) {
+				calculatingResult.innerHTML = +((88.36 + (13.4 * weightValue) + (4.8 * heightValue) - (5.7 * ageValue)) * activityValue).toFixed(0);
+			}
+		}
+	};
+
+	const getBtnData = (parentDiv) => {
+		parentDiv.addEventListener('click', (event) => {
+			const childItems = parentDiv.querySelectorAll('div');
+
+			childItems.forEach(item => {
+				item.classList.remove('calculating__choose-item_active');
+
+				if (item === event.target) {
+					item.classList.add('calculating__choose-item_active');
+				}
+			});
+
+			if (event.target.dataset.activity) {
+				activityValue = event.target.dataset.activity;			
+			} else {
+				sexValue = event.target.id;	
+			}
+
+			calcResult(sexValue, heightValue, weightValue, ageValue, activityValue);
+		});
+	};
+
+	const getInputData = (input) => {
+		input.addEventListener('input', (event) => {
+			input.classList.add('calculating__choose-item_active');
+
+			if (event.target.id === 'height') {
+				heightValue = input.value;
+			} else if (event.target.id === 'weight') {
+				weightValue = input.value;
+			} else if (event.target.id === 'age') {
+				ageValue = input.value;
+			}
+
+			calcResult(sexValue, heightValue, weightValue, ageValue, activityValue);
+		});
+
+	};
+
+	calcResult(sexValue, heightValue, weightValue, ageValue, activityValue);
+
+	getBtnData(sex);
+	getBtnData(activity);
+	getInputData(height);
+	getInputData(weight);
+	getInputData(age);
 });
